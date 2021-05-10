002bc334b1bb907be155a28a1186cc64326326df
osamaHamad0 <>
2021-05-10 16:21:16 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('enter').addEventListener('click', rateMovie)
+
+function movieByAge (age) {
+  if (age >= 17) {
+    return 'You may watch R rated movies'
+  } else if (age >= 13) {
+    return 'You may watch PG-13 rated movies'
+  } else {
+    return 'You may watch G or PG rated movies'
+  }
+}
+
+function rateMovie () {
+  const age = parseInt(document.getElementById('input').value)
+  let answer = document.getElementById('answer')
+
+  answer.innerText = movieByAge(age)
+}
